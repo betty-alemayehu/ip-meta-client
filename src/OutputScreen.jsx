35ffc96576ac83cd736df6f63ba49facb62578da
betelhemalemayehu <Betelhem.L.A@gmail.com>
2024-11-21 20:34:38 -0500

    broilerplate code updated without styling

--- src/OutputScreen.jsx
+++ src/OutputScreen.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import "./OutputScreen.css";
+// import "./OutputScreen.css";
 
 const OutputScreen = ({ joke }) => {
   const navigate = useNavigate();
