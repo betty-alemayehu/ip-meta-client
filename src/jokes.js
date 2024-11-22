2ed6dcb6adb858ed08dd1216d0b5bf2849132aa5
betelhemalemayehu <Betelhem.L.A@gmail.com>
2024-11-21 20:27:51 -0500

    broilerplate code added

---
+++ src/jokes.js
@@ -0,0 +1,23 @@
+export const jokes = [
+  {
+    hobby: "Sports & Fitness",
+    age: "18-25",
+    mood: "Energetic",
+    type: "Sarcastic",
+    joke: "Why did the gym close down? It just didn’t work out!",
+  },
+  {
+    hobby: "Technology & Gaming",
+    age: "26-35",
+    mood: "Reflective",
+    type: "Witty/Intellectual",
+    joke: "I told my computer I needed a break, and now it won’t stop sending me Kit-Kat memes.",
+  },
+  {
+    hobby: "Cooking & Food",
+    age: "36-45",
+    mood: "Happy",
+    type: "Pun-based",
+    joke: "I’m on a seafood diet. I see food, and I eat it!",
+  },
+];
